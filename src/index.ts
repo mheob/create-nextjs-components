cc9e51378e9bfdd9e6e52fa719a36c9cbbcb6cd0
Alexander Böhm <3983539+mheob@users.noreply.github.com>
2022-03-10 19:22:54 +0100

    :sparkles: create a first cli

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('TEST');
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+type Answers = {
+  path: string;
+  story?: boolean;
+  test?: boolean;
+  'dry-run'?: boolean;
+};
+
+export const cli = async (processArgv: string[]) => {
+  yargs(hideBin(processArgv))
+    .usage('Usage: $0 --path="src/components" [--story] [--test]')
+    .option('path', {
+      alias: 'p',
+      describe: 'Path where to generated',
+      demandOption: true,
+      type: 'string',
+    })
+    .option('story', {
+      alias: 's',
+      describe: 'Create additional story file',
+      type: 'boolean',
+    })
+    .option('test', {
+      alias: 't',
+      describe: 'Create additional test file',
+      type: 'boolean',
+    })
+    .option('dry-run', {
+      describe: 'Only display output without generating files',
+      type: 'boolean',
+    })
+    .help()
+    .version().argv;
+};
